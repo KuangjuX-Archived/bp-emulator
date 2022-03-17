e5b0a87e9cd54023189b6504031ea8e780c9f6d1
KuangjuX <18630816527@163.com>
2022-03-17 14:51:21 +0800

    add example0

---
+++ src/bin/gshare.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::env;
+use regex::Regex;
+use bp_emulator::{ Predictor, GShareBranchPredictor };
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() < 4 {
+        panic!("[Error] Least three arguments")
+    }
+    let m = usize::from_str_radix(args[1].as_str(), 10).unwrap();
+    let n = usize::from_str_radix(args[2].as_str(), 10).unwrap();
+    let trace = &args[3];
+    
+    let mut bp = GShareBranchPredictor::new(m, n);
+    let file = File::open(trace).unwrap();
+    let reader = BufReader::new(file);
+    let mut line_counts = 0;
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            line_counts += 1;
+            let pattern = Regex::new(r"([0-9a-fA-F]+) ([a-zA-Z])").unwrap();
+            let cap = pattern.captures(&line).unwrap();
+            let pc = usize::from_str_radix(&cap[1], 16).unwrap();
+            let res = match &cap[2] {
+                "t" => { true },
+                "n" => { false },
+                _ => panic!("[Error] Invalid result")
+            };
+            bp.predict(pc, res);
+            if line_counts >= 2000 {
+                break;
+            }
+        }
+    }
+    bp.print_res();
+}
